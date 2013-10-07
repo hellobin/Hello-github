3d1a7077de8e995c544fffd9a8919c29a7229154
icenan2 <icenan2@gmail.com>
2013-10-07 21:26:45 +0800

    test

--- java.java
+++ java.java
@@ -2,7 +2,5 @@ public class Hello{
     public static void main()
     {
         System.out.println("Hello github!");
-        System.out.printf("Hello world!\n");
-        System.out.println("Hello world!");
     }
 }
